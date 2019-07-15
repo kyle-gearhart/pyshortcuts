d14fb501fc28e4d3c61887f70b152cd0326e2212
kyle-gearhart <kyle.gearhart@2book.com>
2019-07-15 15:26:15 -0400

    Add Modules

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='pyshortcuts',
       author='Kyle A Gearhart',
       author_email='kagearhart@gmail.com',
       license='GPLv3',
-      packages=['pyshortcuts'],
+      packages=['pyshortcuts', 'pyshortcuts.cl', 'pyshortcuts.csv', 'pyshortcuts.db'],
       install_requires=['pymysql', 'sshtunnel'])
